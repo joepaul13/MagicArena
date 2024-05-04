55d50863721e022342f816bbb177aef1ec0e0712
joepaul13 <tjoepaul@outlook.com>
2024-05-04 11:03:22 +0530

    Simulation Code

---
+++ matchSimulation.js
@@ -0,0 +1,19 @@
+function simulateMatch(playerA, playerB) {
+  let attackingPlayer = playerA.health < playerB.health ? playerA : playerB;
+  let defendingPlayer = attackingPlayer === playerA ? playerB : playerA;
+
+  while (attackingPlayer.health > 0 && defendingPlayer > 0) {
+    const attackingDiceRoll = Math.floor(Math.random() * 6) + 1;
+    const defendingDiceRoll = Math.floor(Math.random() * 6) + 1;
+
+    const attackDamage = attackingPlayer.getAttackDamage(attackingDiceRoll);
+    const defendedDamage = defendingPlayer.strength * defendingDiceRoll;
+
+    const damageDealt = Math.max(attackDamage - defendedDamage, 0);
+    defendingPlayer.takeDamage(damageDealt);
+
+    [attackingPlayer, defendingPlayer] = [defendingPlayer, attackingPlayer];
+  }
+
+  return attackingPlayer.health === 0 ? defendingPlayer : attackingPlayer;
+}
---
+++ player.js
@@ -0,0 +1,18 @@
+class Player {
+  constructor(health, strength, attack) {
+    this.health = health;
+    this.strength = strength;
+    this.attack = attack;
+  }
+
+  getAttackDamage(diceRoll) {
+    return this.attack * diceRoll;
+  }
+
+  takeDamage(damage) {
+    this.health -= damage;
+    if (this.health < 0) {
+      this.health = 0;
+    }
+  }
+}
